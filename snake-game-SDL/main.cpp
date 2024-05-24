31cd7b7c42c79834153ac313c99320295a62fe23
jibril <100243649+09jayy@users.noreply.github.com>
2024-05-24 02:20:23 +0100

    Created game loop framerate cap
    ensures frame rate remains consistent throughout

--- snake-game-SDL/main.cpp
+++ snake-game-SDL/main.cpp
@@ -3,13 +3,26 @@
 Game* game = nullptr; 
 
 int main(int argc, char* argv[]) {
+
+    const int FPS = 30; 
+
+    Uint32 elapsed = 0; 
+    int frameTime = 0; 
+
     game = new Game(); 
-    game->init("Snake-Game by 09Jayy", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false); 
+    game->init("Snake-Game by 09Jayy", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, false); 
 
     while (game->running()) {
+        elapsed = SDL_GetTicks(); 
         game->handleEvents(); 
         game->update(); 
         game->render(); 
+
+        frameTime = SDL_GetTicks() - elapsed; 
+
+        if (1000 / FPS > frameTime) {
+            SDL_Delay((1000 / FPS) - frameTime);
+        }; 
     }
     game->clean(); 
 
