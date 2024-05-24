02564e8d1a71f4655229c132cfbd99bb6e591ae7
jibril <100243649+09jayy@users.noreply.github.com>
2024-05-24 19:26:38 +0100

    added surface attribute and setPosition method to text class
    this allows the position to be set after an object has been initialised but before it is rendered. This can allow for preset placements such as center better

--- snake-game-SDL/Text.cpp
+++ snake-game-SDL/Text.cpp
@@ -5,40 +5,27 @@ Text::~Text() {
 		TTF_CloseFont(font);
 	}; 
 }; 
-Text::Text(const char* text, const char* fontPath, int fontSize) : text(text){
+Text::Text(const char* text, const char* fontPath, int fontSize) : text(text), x(0), y(0){
 	font = TTF_OpenFont(fontPath, fontSize); 
 }; 
 
-Text::Text(const char*, const char* fontPath, int fontSize, const char* position) : text(text) {
-	font = TTF_OpenFont(fontPath, fontSize);
+void Text::setPosition(int x, int y) {
+	sur = TTF_RenderText_Solid(font, text, SDL_Color{ 255,255,255 });
+	this->x = x;
+	this->y = y;
 }
 
-void Text::render(SDL_Renderer* renderer,int x, int y) {
-	//create texture
-	SDL_Surface* sur = TTF_RenderText_Solid(font, text, SDL_Color{ 255,255,255 });
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, sur);
-	SDL_Rect rect = { x,y,sur->w, sur->h };
-	SDL_FreeSurface(sur);
-
-	//render
-	SDL_RenderCopy(renderer, texture, NULL, &rect);
-
-	SDL_DestroyTexture(texture);
-}
-
-void Text::render(SDL_Renderer* renderer, const char* position) {
-	int x = 0; 
-	int y = 0; 
-	
-	//create texture
-	SDL_Surface* sur = TTF_RenderText_Solid(font, text, SDL_Color{ 255,255,255 });
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, sur);
-
+void Text::setPosition(const char* position) {
+	sur = TTF_RenderText_Solid(font, text, SDL_Color{ 255,255,255 });
 	if (position == "CENTER") {
-		x = 400 - sur->w / 2; 
-		y = 400 - sur->h / 2; 
-	} 
+		x = 400 - sur->w / 2;
+		y = 400 - sur->h / 2;
+	}
+}
 
+void Text::render(SDL_Renderer* renderer) {
+	//create texture
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, sur);
 	SDL_Rect rect = { x,y,sur->w, sur->h };
 	SDL_FreeSurface(sur);
 
--- snake-game-SDL/Text.h
+++ snake-game-SDL/Text.h
@@ -6,14 +6,16 @@
 class Text
 {
 private:
-	const char* text; 
+	const char* text;
 	TTF_Font* font;
+	SDL_Surface* sur;
+	int x, y;
 public:
-	Text(const char*, const char* fontPath, int fontSize); 
-	Text(const char*, const char* fontPath, int fontSize, const char* position);
-	~Text(); 
+	Text(const char*, const char* fontPath, int fontSize);
+	~Text();
 
-	void render(SDL_Renderer* renderer, int x, int y); 
-	void render(SDL_Renderer* renderer, const char* position);
-};
+	void setPosition(int x, int y);
+	void setPosition(const char* position);
+	void render(SDL_Renderer* renderer);
+}; 
 
